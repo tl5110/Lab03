ff2ed6ddaa497766d4d22b226e5f3550b9ea2a23
Tiffany Lee <tl5110@g.rit.edu>
2023-02-01 17:46:38 -0500

    Finished implementing StackNode

--- src/rit/stu/StackNode.java
+++ src/rit/stu/StackNode.java
@@ -9,30 +9,57 @@ import rit.cs.Stack;
  * @param <T> The type of data the stack will hold
  * @author RIT CS
  * @author Sean Strout
+ * @author Tiffany Lee
  */
 public class StackNode<T> implements Stack<T> {
+    Node<T> top = new Node<T>(null, null);
 
     /**
      * Create an empty stack.
      */
     public StackNode() {
+        this.top = null;
     }
 
+    /**
+     * Checks if the stack is currently empty or not
+     * @return true if empty, false otherwise
+     */
     @Override
     public boolean empty() {
-        return false;
+        return top.getData() == null;
     }
 
+    /**
+     *Remove and return the top element in the stack
+     * @pre stack must not be empty
+     * @throws AssertionError if stack is empty
+     * @return the front element
+     */
     @Override
     public T pop() {
-        return null;
+        assert !empty();
+        T og = top.getData();
+        top.setData(top.getNext().getData());
+        return og;
     }
 
+    /**
+     * Adds a new element to the top fo the stack
+     * @param element The new data element
+     */
     @Override
-    public void push(T element) {}
+    public void push(T element) {
+        top.setNext(top);
+        top.setData(element);
+    }
 
+    /**
+     * Gets the top element of the stack
+     * @return the top element
+     */
     @Override
     public T top() {
-        return null;
+        return top.getData();
     }
 }
